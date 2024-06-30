fad39a3368e640431337dd22732c8d8b05abce61
Darren B <68653294+Devd0@users.noreply.github.com>
2024-06-30 17:51:48 -0400

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export function helloWorld() {
-	return 'helloWorld';
-}
+import type { AstroIntegration, AstroIntegrationLogger } from 'astro';
+
+const INTEGRATION_NAME = 'astro-ai-frontmatter';
+
+interface Options {}
+
+const plugin = (options: Options): AstroIntegration => {
+	return {
+		name: INTEGRATION_NAME,
+		hooks: {
+			'astro:build:setup': async ({ pages, logger }) => {
+				logger.info('[astro-ai-frontmatter] Starting...');
+			},
+		},
+	};
+};
+
+export default plugin;
